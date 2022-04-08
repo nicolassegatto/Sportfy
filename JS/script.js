cd4735d1ff8023d8247e30cabeb8fb3468e928ca
Nicolas Segatto <nicolasssegatto@icloud.com>
2022-04-08 02:09:11 -0300

    full responsive

--- JS/script.js
+++ JS/script.js
@@ -8,20 +8,15 @@ class MobileNavbar {
         this.handleClick = this.handleClick.bind(this);
     }
 
-    /*animateLinks() {
-        this.navLinks.forEach((link, index) => {
-          link.style.animation
-            ? (link.style.animation = "")
-            : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-                index / 7 + 0.4
-              }s`);
-        });
-      }*/
-
     handleClick() {
         this.navList.classList.toggle(this.activeClass);
         this.mobileMenu.classList.toggle(this.activeClass);
-        /*this.animateLinks();*/
+        let over = document.querySelector("body").style.overflow
+        if(over == "hidden") {
+            document.querySelector("body").style.overflow = ""
+        }else{
+            document.querySelector("body").style.overflow = "hidden"
+        }
     }
 
     addClickEvent(){
